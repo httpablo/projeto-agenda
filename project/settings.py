38f20aed5e709ed162df43809e3230a0071e9fb9
Pablo Brito <pablobrith@gmail.com>
2024-09-06 17:50:57 -0300

    models product

--- project/settings.py
+++ project/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-          BASE_DIR / 'base_templates'
+            BASE_DIR / 'base_templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -123,7 +123,18 @@ STATICFILES_DIRS = (
   BASE_DIR / 'base_static',
 )
 
+STATIC_ROOT = BASE_DIR / 'static' #collectstatic
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+  from project.local_settings import *
+except ImportError:
+  ...
