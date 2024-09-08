0d07659aa4e9d22a33e330961917b531de07d2c2
Pablo Brito <pablobrith@gmail.com>
2024-09-08 20:35:31 -0300

    html create

--- contact/urls.py
+++ contact/urls.py
@@ -6,4 +6,9 @@ app_name = 'contact'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
+
+    # contact CRUD
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
 ]
